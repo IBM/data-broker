c3bc375e452adb662a1994ca4c94420eec0ae888
Claudia Misale <34547329+cmisale@users.noreply.github.com>
2019-11-04 08:55:21 -0500

    Update errorcodes.py

--- bindings/python/dbr_module/errorcodes.py
+++ bindings/python/dbr_module/errorcodes.py
@@ -42,8 +42,8 @@ typedef enum {
   DBR_ERR_INVALIDOP, // invalid operation
   DBR_ERR_BE_POST, // posting request to back-end failed
   DBR_ERR_BE_GENERAL, // Unspecified back-end error
-  DBR_ERR_ITERATOR,
-  DBR_ERR_PLUGIN,
+  DBR_ERR_ITERATOR, // Invalid iterator or error iterating the namespace
+  DBR_ERR_PLUGIN, // Data adapter error
   DBR_ERR_MAXERROR
 } DBR_Errorcode_t;
 
@@ -70,6 +70,8 @@ char *dbrError_table[ DBR_ERR_MAXERROR ] = {
    [ DBR_ERR_NOTIMPL ] = "Operation not implemented",
    [ DBR_ERR_INVALIDOP ] = "Invalid operation",
    [ DBR_ERR_BE_POST ] = "Failed to post request to back-end",
+   [ DBR_ERR_ITERATOR ] = "Invalid iterator or error iterating the namespace",
+   [ DBR_ERR_PLUGIN ] = "Data adapter error",
    [ DBR_ERR_BE_GENERAL ] = "Unspecified back-end error"
 };
 
@@ -105,9 +107,9 @@ typedef enum {
   DBR_ERR_INVALIDOP, // invalid operation
   DBR_ERR_BE_POST, // posting request to back-end failed
   DBR_ERR_BE_GENERAL, // Unspecified back-end error
-  DBR_ERR_ITERATOR,
-  DBR_ERR_PLUGIN,
-  DBR_ERR_MAXERROR,
+  DBR_ERR_ITERATOR, // Invalid iterator or error iterating the namespace
+  DBR_ERR_PLUGIN, // Data adapter error
+  DBR_ERR_MAXERROR
 } DBR_Errorcode_t;
 
 
